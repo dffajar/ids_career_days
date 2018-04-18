9ae3051b403cf72c4ddc52db6d02183e44c8432a
Dicki Fadilah Fajar <fadilahfajar74@gmail.com>
2018-04-18 13:40:16 +0700

    Ganti

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Zeeber
+                    Career Days
                 </div>
 
                 <div class="links">
