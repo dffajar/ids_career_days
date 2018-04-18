d94ea198b7109c873121711d8a1e887a90698b7e
Dicki Fadilah Fajar <fadilahfajar74@gmail.com>
2018-04-18 13:52:11 +0700

    Lihat

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Zeeber
+                    Wibu Lo
                 </div>
 
                 <div class="links">
