ba455f8c715270d74024b8293e29436526b38920
yohanesrandy <randykurniayanto@gmail.com>
2018-04-18 13:42:25 +0700

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Zeeber
                 </div>
 
                 <div class="links">
